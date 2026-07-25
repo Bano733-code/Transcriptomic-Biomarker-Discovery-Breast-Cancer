d4cfa8a8793e6a50f3bb282d40c09fe8d056e604
Bano Rani <banor3106@gmail.com>
2026-07-25 15:37:51 +0500

    Update GO_KEGG_analysis.R

--- scripts/GO_KEGG_analysis.R
+++ scripts/GO_KEGG_analysis.R
@@ -1,128 +1,135 @@
 library(clusterProfiler)
 library(enrichplot)
 library(org.Hs.eg.db)
+library(ggplot2)
+library(stringr)
 
 
 load("results_objects.RData")
 
 
+# Significant genes
+
 sig_genes <- results[
-  results$adj.P.Val <0.05 &
-    abs(results$logFC)>1,
+  results$adj.P.Val < 0.05 &
+    abs(results$logFC) > 1,
 ]
 
 
 genes <- sig_genes$GeneSymbol
 
 
+# Convert SYMBOL to ENTREZ ID
+
 gene_ids <- bitr(
   genes,
-  fromType="SYMBOL",
-  toType="ENTREZID",
-  OrgDb=org.Hs.eg.db
+  fromType = "SYMBOL",
+  toType = "ENTREZID",
+  OrgDb = org.Hs.eg.db
 )
+
+
+
+############################
+# GO ENRICHMENT
+############################
+
 
 ego <- enrichGO(
-  gene=gene_ids$ENTREZID,
-  OrgDb=org.Hs.eg.db,
-  ont="BP",
-  pAdjustMethod="BH"
+  gene = gene_ids$ENTREZID,
+  OrgDb = org.Hs.eg.db,
+  ont = "BP",
+  pAdjustMethod = "BH",
+  pvalueCutoff = 0.05
 )
 
-go_plot <- dotplot(
-  ego,
-  showCategory = 10,
-  font.size = 12
-) +
-  ggtitle("Gene Ontology Biological Process Enrichment") +
-  theme(
-    plot.title = element_text(
-      size = 14,
-      face = "bold"
-    )
+
+go_df <- as.data.frame(ego)
+
+
+go_df <- go_df[1:10,]
+
+
+go_df$Description <- str_wrap(
+  go_df$Description,
+  width = 45
+)
+
+
+go_plot <- ggplot(
+  go_df,
+  aes(
+    x = reorder(Description, -log10(p.adjust)),
+    y = -log10(p.adjust)
   )
+)+
+geom_bar(stat="identity")+
+coord_flip()+
+theme_classic()+
+labs(
+title="GO Biological Process Enrichment",
+x="",
+y="-log10 Adjusted P-value"
+)
+
 
 go_plot
 
 
 ggsave(
-  "results/figures/GO_dotplot.png",
-  go_plot,
-  width = 10,
-  height = 12,
-  dpi = 300
+"results/figures/GO_barplot.png",
+go_plot,
+width=10,
+height=7,
+dpi=300
 )
+
+
+
+############################
+# KEGG ENRICHMENT
+############################
+
 
 kegg_result <- enrichKEGG(
-  gene=gene_ids$ENTREZID,
-  organism="hsa"
+  gene = gene_ids$ENTREZID,
+  organism = "hsa",
+  pvalueCutoff = 0.05
 )
 
 
-dotplot(kegg_result)
 
 kegg_plot <- dotplot(
   kegg_result,
   showCategory = 10,
   font.size = 10
-) +
-  ggtitle("KEGG Pathway Enrichment Analysis") +
-  theme(
-    plot.title = element_text(
-      size = 14,
-      face = "bold"
-    )
-  )
+)+
+ggtitle(
+"KEGG Pathway Enrichment Analysis"
+)+
+theme(
+plot.title = element_text(
+size=14,
+face="bold"
+)
+)
 
 
 kegg_plot
 
 
 ggsave(
-  "results/figures/KEGG_dotplot.png",
-  kegg_plot,
-  width = 10,
-  height = 7,
-  dpi = 300
+"results/figures/KEGG_dotplot.png",
+kegg_plot,
+width=10,
+height=7,
+dpi=300
 )
 
 
-save(ego,kegg_result,
-     file="GO_KEGG_results.RData")
 
-kegg_result <- enrichKEGG(
-  gene=gene_ids$ENTREZID,
-  organism="hsa"
+save(
+ego,
+kegg_result,
+file="GO_KEGG_results.RData"
 )
-
-
-dotplot(kegg_result)
-
-kegg_plot <- dotplot(
-  kegg_result,
-  showCategory = 10,
-  font.size = 10
-) +
-  ggtitle("KEGG Pathway Enrichment Analysis") +
-  theme(
-    plot.title = element_text(
-      size = 14,
-      face = "bold"
-    )
-  )
-
-
-kegg_plot
-
-
-ggsave(
-  "results/figures/KEGG_dotplot.png",
-  kegg_plot,
-  width = 10,
-  height = 7,
-  dpi = 300
-)
-
-
-save(ego,kegg_result,
-     file="GO_KEGG_results.RData")
